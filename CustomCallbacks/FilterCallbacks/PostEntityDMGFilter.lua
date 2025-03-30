4c5f1af382aac0028b9c5de0c42dba37a6585571
Syntax-Sculptor <SyntaxSculptor@proton.me>
2025-03-30 12:12:48 -0800

    Update PostEntityDMGFilter.lua

--- CustomCallbacks/FilterCallbacks/PostEntityDMGFilter.lua
+++ CustomCallbacks/FilterCallbacks/PostEntityDMGFilter.lua
@@ -7,7 +7,7 @@ TSIL.__RegisterCustomCallback(
 
 
 local function OnEntityDamage(_, entity, amount, flags, source, countdown)
-    return TSIL.__TriggerCustomCallback(TSIL.Enums.CustomCallback.ENTITY_TAKE_DMG_FILTER, entity, amount, flags, source, countdown)
+    return TSIL.__TriggerCustomCallback(TSIL.Enums.CustomCallback.POST_ENTITY_TAKE_DMG_FILTER, entity, amount, flags, source, countdown)
 end
 TSIL.__AddInternalCallback(
     "POST_ENTITY_DMG_FILTER_CALLBACK_POST_ENTITY_DMG",
